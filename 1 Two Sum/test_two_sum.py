9ad0be83b6776d9234c54a1aae2c66c3b191d667
RecheckCarrOil <charliemcrocker@gmail.com>
2021-04-28 23:18:21 +0100

    Changed to dict solution, not actually faster

--- 1 Two Sum/test_two_sum.py
+++ 1 Two Sum/test_two_sum.py
@@ -4,5 +4,5 @@ from two_sum import *
 
 @pytest.mark.parametrize("input_list, target, result",
                          [([2, 7, 11, 15], 9, [0, 1]), ([3, 2, 4], 6, [1, 2]), ([3, 3], 6, [0, 1])])
-def test_twoSum(input_list, target, result):
+def test_two_sum(input_list, target, result):
     assert Solution.twoSum(input_list, target) == result
--- 1 Two Sum/two_sum.py
+++ 1 Two Sum/two_sum.py
@@ -4,19 +4,8 @@ from typing import List
 class Solution:
     @staticmethod
     def twoSum(nums: List[int], target: int) -> List[int]:
-        for ind in range(len(nums)):
-            nums_copy = nums.copy()
-            nums_copy.pop(ind)
-            for num in nums_copy:
-                if nums[ind] + num == target:
-                    return [ind, nums.index(num, ind+1)]
-
-
-if __name__ == "__main__":
-    instance = Solution()
-    nums = [-1,-2,-3,-4,-5]
-    target = -8
-    result = instance.twoSum(nums, target)
-
-    print(result)
-
+        other_halves = {}
+        for i, num in enumerate(nums):
+            if num in other_halves:
+                return [other_halves[num], i]
+            other_halves[target - num] = i
